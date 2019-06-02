d17d2609551fb315cea66b5eea23901b43f431ee
David Fentz <bomjumaku@gmail.com>
2019-06-02 08:19:48 -0500

    commiting previous work, not sure what I did.

--- simple_hr/lib/main.dart
+++ simple_hr/lib/main.dart
@@ -1,3 +1,8 @@
+//TODO: if maxHR<minHR, give the user an indicator. If trying to start,
+// throw an error popup. 
+
+
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Simple HR',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.orange,
       ),
       home: MyHomePage(title: 'Simple HR'),
     );
@@ -28,65 +33,43 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _minHR = 0;
   int _maxHR = 0;
-  int _counter = 0;
+  int _currentHR = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _start(){
+    // start pinging the Polar HR monitor and checking if my HR is lower or
+    // higher than it should be.
+    print("I feel like something should be happening...");
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title),),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            TextField(
+              decoration:
+                  InputDecoration(labelText: 'Maximum Heart Rate'),
+              onChanged: (val) =>
+                  setState(() => _maxHR = int.parse(val)),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            TextField(
+              decoration:
+                  InputDecoration(labelText: 'Minimum Heart Rate'),
+              onChanged: (val) =>
+                  setState(() => _minHR = int.parse(val)),
             ),
+            Text('Maximum Heart Rate: ' + _maxHR.toString(),),
+            Text('Minimum Heart Rate: ' + _minHR.toString(),),
+            Text('Current Heart Rate: ' + _currentHR.toString(),),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _start,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
